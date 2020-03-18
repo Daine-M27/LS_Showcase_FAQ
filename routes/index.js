f4f94f21625882da34010906775e13e321d94292
Daine Marshall <dmarshall@thelightsource.com>
2020-03-18 16:27:06 -0400

    accordian working

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var router = express.Router();
-var faqData = require("../JSON/faqData");
+// load questions and answers from json file into variable for template engine
+var faqData = require("./../JSON/faqData");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { faqData });
+  res.render('index', { questions: faqData, title: "The Light Source Showcase" });
 });
 
 module.exports = router;
