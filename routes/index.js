890f6b8645220d889e9f88fefaee25711ed0a8c1
Daine Marshall <dmarshall@thelightsource.com>
2020-03-18 15:04:01 -0400

    added json file

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var faqData = require("../JSON/faqData");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'The Light Source Showcase' });
+  res.render('index', { faqData });
 });
 
 module.exports = router;
