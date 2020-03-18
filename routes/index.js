e3923012037b91b74f15a17f5b5e1381cb8bc5b6
Daine Marshall <dmarshall@thelightsource.com>
2020-03-18 19:39:51 -0400

    images

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,11 @@ var express = require('express');
 var router = express.Router();
 // load questions and answers from json file into variable for template engine
 var faqData = require("./../JSON/faqData");
+var slideData = require("./../JSON/slideShow");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { questions: faqData, title: "The Light Source Showcase" });
+  res.render('index', { title: "The Light Source Showcase", questions: faqData, allSlides: slideData });
 });
 
 module.exports = router;
