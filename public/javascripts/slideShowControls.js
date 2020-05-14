b4753e852ca4b7be87abab9240ab0f89f23ae8b2
Daine Marshall <dmarshall@thelightsource.com>
2020-05-14 15:58:53 -0400

    correct gymLight image

--- public/javascripts/slideShowControls.js
+++ public/javascripts/slideShowControls.js
@@ -1,8 +1,35 @@
-var slideIndex = 1;
+// var slideIndex = 0;
+// showSlides();
+
+// function showSlides() {
+//   var i;
+//   var slides = document.getElementsByClassName("mySlides");
+//   //var dots = document.getElementsByClassName("dot");
+//   for (i = 0; i < slides.length; i++) {
+//     slides[i].style.display = "none";  
+//   }
+//   slideIndex++;
+//   if (slideIndex > slides.length) {slideIndex = 1}    
+//   //for (i = 0; i < dots.length; i++) {
+//    // dots[i].className = dots[i].className.replace(" active", "");
+//   //}
+//   slides[slideIndex-1].style.display = "block";  
+//   //dots[slideIndex-1].className += " active";
+//   setTimeout(showSlides, 10000); // Change image every 2 seconds
+// }
+//////////////////////////////////////////////////
+var slideIndex = 0;
 showSlides(slideIndex);
+setInterval(plusSlides(slideIndex), 1500);
 
 function plusSlides(n) {
-  showSlides(slideIndex += n);
+  var slides = document.getElementsByClassName("mySlides");
+  if(slideIndex > slides.length){
+    slideIndex = 1;
+    showSlides(slideIndex)
+  }else{
+    showSlides(slideIndex = n+1);
+  }
 }
 
 function currentSlide(n) {
@@ -22,6 +49,6 @@ function showSlides(n) {
       dots[i].className = dots[i].className.replace(" dot-active", "");
   }
   slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " dot-active";
+  dots[slideIndex-1].className += " dot-active";  
 }
 
